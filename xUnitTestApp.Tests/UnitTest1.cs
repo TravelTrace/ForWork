7fc45ea59838c85037aa66b3c5aaec1f6a447264
LeoCodeRu <leo@leocode.ru>
2019-06-18 12:28:30 +0300

    Добавлен модуль тестов xUnit

---
+++ xUnitTestApp.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using HomeWork;
+using System;
+using Xunit;
+
+namespace xUnitTestApp.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Show_All_Realtors()
+        {
+            Realtor target = new Realtor();
+            //target.
+        }
+    }
+}
